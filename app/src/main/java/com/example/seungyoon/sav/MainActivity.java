068545f6d98b39382415f643b530121297300968
ACM-ICPC <ACM-ICPC>
2016-11-24 21:23:05 +0900

    Naver speech recognition

--- app/src/main/java/com/example/seungyoon/sav/MainActivity.java
+++ app/src/main/java/com/example/seungyoon/sav/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.seungyoon.sav;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import com.example.seungyoon.sav.naverTalk.NaverTalkActivity;
+
+public class MainActivity extends Activity {
+
+    private Button voiceBtn;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        voiceBtn = (Button)findViewById(R.id.btn_voice);
+
+
+
+        voiceBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, NaverTalkActivity.class);
+
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
